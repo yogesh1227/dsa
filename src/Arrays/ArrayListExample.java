18e6e104c312bc8bde072aafb069cde19ca00612
yogesh1227 <yogeshkakkar521@gmail.com>
2024-03-02 22:56:18 -0500

    Arraylist program

---
+++ src/Arrays/ArrayListExample.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(55);
+        arr.add(56);
+        arr.add(57);
+        arr.add(51);
+        arr.add(52);
+        arr.add(53);
+        arr.add(54);
+        System.out.println(arr);
+
+    }
+}
